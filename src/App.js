3f4af4fd49a578850e5af738b8246581da89c143
ncasale <ncasale@ncsu.edu>
2018-01-16 14:30:48 -0500

    add card as review basis, see trello for next steps

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
+/*
+
+Jan 15 2018
+
+lyre website
+
+*/
+
 import React, { Component } from "react";
 import "./App.css";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 // for custom muiTheme
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {
-  cyan500, cyan700,
-  pinkA200,
-  grey100, grey300, grey400, grey500,
-  white, darkBlack, fullBlack,
-} from 'material-ui/styles/colors';
+// for cards
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 // trying to do https://stackoverflow.com/questions/23258421/how-to-stop-app-that-node-js-express-npm-start
 // see package.json for the added 'stop' command
 process.title = App;
 
-// This replaces the textColor value on the palette
-// and then update the keys for each component that depends on it.
-// More on Colors: http://www.material-ui.com/#/customization/colors
+/*
+custom muiTheme
+More on Colors: http://www.material-ui.com/#/customization/colors
+*/
 const muiTheme = getMuiTheme({
+  fontFamily: 'Helvetica', //sans-serif',
   palette: {
-    primary1Color: '#000000',
+    primary1Color: '#332222', // appbar background color
     textColor: '#000000',
-    canvasColor: fullBlack,
+    alternateTextColor: '#ffffff', // appbar text color
   },
   appBar: {
     height: 100,
   },
+  paper: {
+    height: 200,
+    width: '50%',
+    margin: 200,
+    textAlign: 'center',
+    display: 'inline-block',
+  },
 });
-
-/*
-// inline style reference
-style={{
-  width: '100%',
-  margin: '2 auto',
-  border: '0px solid #000000',
-  backgroundColor: '#442222',
-
-}}
-*/
 
 class App extends Component {
   render() {
@@ -48,6 +51,29 @@ class App extends Component {
           title="lyre"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
         />
+
+        <div style={{width: '60%', margin: '0 auto'}}>
+          <Card style={{width:'100%', justifyContent: 'center'}}>
+            <CardHeader
+              title="reviewer name"
+              subtitle="subtitle"
+              avatar="./images/jens.jpg"
+            />
+            <CardMedia
+              //overlay={<CardTitle title="review title" subtitle="review subtitle" />}
+            >
+              <img src="./images/jens.jpg" alt="./images/jens.jpg" />
+            </CardMedia>
+            <CardTitle title="other title" subtitle="other subtitle" />
+            <CardText>
+              this is a scathing review of jen's lekman's album Life Will See You Now
+            </CardText>
+            <CardActions>
+              <FlatButton label="Action1" />
+              <FlatButton label="Action2" />
+            </CardActions>
+          </Card>
+        </div>
       </MuiThemeProvider>
     );
   }
