cf54d7117a7efbc3365631a73716741a9b6297fd
Alex Williams <alexwilliams567@gmail.com>
2018-01-26 12:59:32 -0500

    componetizing review card, creating stateful review details

--- src/App.js
+++ src/App.js
@@ -12,22 +12,8 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 // for custom muiTheme
 import getMuiTheme from "material-ui/styles/getMuiTheme";
-// for cards
-import {
-  Card,
-  CardActions,
-  CardHeader,
-  CardMedia,
-  CardTitle,
-  CardText
-} from "material-ui/Card";
 import FlatButton from "material-ui/FlatButton";
-import Avatar from "material-ui/Avatar";
-
-// import images
-import jens from "./images/jens.jpg";
-import lifewillseeyounow from "./images/lifewillseeyounow.jpg";
-import ruins from "./images/ruins.jpg";
+import ReviewCard from "./components/ReviewCard";
 
 // trying to do https://stackoverflow.com/questions/23258421/how-to-stop-app-that-node-js-express-npm-start
 // see package.json for the added 'stop' command
@@ -50,58 +36,14 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <AppBar className="App-header" title="lyre" />
-
-        {/* to add white space after floating/fixed appbar */}
-        {/* <span>
-          {" "}
-          <br />
-          <br />
-          <br />
-        </span> */}
+        <AppBar
+          className="App-header"
+          title="lyre"
+          style={{ position: "fixed" }}
+        />
 
         <div className="reviews-list">
-          <Card className="review-card">
-            <div className="review-card-img">
-              <CardMedia>
-                <img src={lifewillseeyounow} />
-              </CardMedia>
-            </div>
-            <div className="review-card-details">
-              <CardHeader
-                className="review-card__header"
-                title="fens lake-man"
-                subtitle="staff"
-              >
-                <Avatar src={jens} />
-              </CardHeader>
-              <CardTitle title="Life Will See You Now" subtitle="Jens Lekman" />
-              <CardText>
-                this is a scathing review of jens lekmans album Life Will See
-                You Now
-              </CardText>
-            </div>
-          </Card>
-
-          <Card className="review-card">
-            <CardMedia>
-              <img src={ruins} />
-            </CardMedia>
-
-            <CardTitle
-              className="review-card__title"
-              title="Ruins"
-              subtitle="NOT Grouper"
-            />
-            <CardHeader
-              className="review-card__header"
-              title="angry lydia lunch"
-              subtitle="still angry"
-            >
-              <Avatar src={ruins} />
-            </CardHeader>
-            <CardText>ruins is good</CardText>
-          </Card>
+          <ReviewCard />
         </div>
       </MuiThemeProvider>
     );
---
+++ src/components/ReviewCard.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui/Card";
+
+import Avatar from "material-ui/Avatar";
+
+// import images
+import jens from "../images/jens.jpg";
+import lifewillseeyounow from "../images/lifewillseeyounow.jpg";
+
+class ReviewCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExpanded: false
+    };
+  }
+
+  toggleExpand = () => {
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  };
+
+  render() {
+    let notExpandedDetailContentStyle = {
+      maxHeight: "250px",
+      overflow: "hidden"
+    };
+    let expandedDetailContentStyle = {
+      maxHeight: "100%"
+    };
+    return (
+      <Card className="review-card">
+        <div className="review-card-img">
+          <CardMedia>
+            <img src={lifewillseeyounow} style={{ maxHeight: "500px" }} />
+          </CardMedia>
+        </div>
+        <div className="review-card-details">
+          <CardHeader
+            className="review-card__header"
+            title="fens lake-man"
+            subtitle="staff"
+          >
+            <Avatar src={jens} />
+          </CardHeader>
+          <CardTitle title="Life Will See You Now" subtitle="Jens Lekman" />
+          <CardText
+            style={
+              this.state.isExpanded
+                ? expandedDetailContentStyle
+                : notExpandedDetailContentStyle
+            }
+            className="review-card-details__content"
+            onClick={this.toggleExpand}
+          >
+            this is a scathing review of jens lekmans album Life Will See You
+            Now this is a scathing review of jens lekmans album Life Will See
+            You lifewillseeyounowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowu lifewillseeyounowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowu lifewillseeyounowthis is a
+            scathing review of jens lekmans album Life Will See You Nowthis is a
+            scathing review of jens lekmans album Life Will See You Nowthis is a
+            scathing review of jens lekmans album Life Will See You Nowthis is a
+            scathing review of jens lekmans album Life Will See You Nowthis is a
+            scathing review of jens lekmans album Life Will See You Nowthis is a
+            scathing review of jens lekmans album Life Will See You Nowu
+            lifewillseeyounowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowthis is a scathing review of jens lekmans album
+            Life Will See You Nowu lifewillseeyounowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Nowthis is a scathing review of
+            jens lekmans album Life Will See You Now
+          </CardText>
+        </div>
+      </Card>
+    );
+  }
+}
+
+export default ReviewCard;
