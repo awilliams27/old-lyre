0437689cd373e67a8f25e289a29017f9e785ec38
ncasale <ncasale@ncsu.edu>
2018-02-06 19:25:52 -0500

    small changes left over from 8 days ago

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -43,7 +43,7 @@ class ReviewCard extends Component {
       <Card className="review-card">
         <div className="review-card-img">
           <CardMedia>
-            <img src={lifewillseeyounow} style={{ maxWidth: "500px", maxHeight: "500px" }} />
+            <img src={lifewillseeyounow} style={{minWidth: "500px", maxWidth: "500px", minHeight:"500px", maxHeight: "500px" }} />
           </CardMedia>
         </div>
         <div className="review-card-details">
